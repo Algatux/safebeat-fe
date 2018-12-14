9df7fda583cadd7573ec39f329c8a6368dd62909
Alessandro Galli <a.galli85@gmail.com>
2018-12-14 23:26:56 +0100

    removed config.json files

--- src/app/Configuration/configuration.service.ts
+++ src/app/Configuration/configuration.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { LoggerService } from '../Services/logger.service';
+import { environment } from 'src/environments/environment';
 
 interface Configuration {
     appBaseUrl: string;
-}
-
-enum ConfUrl {
-    configFile = 'assets/config.json'
 }
 
 @Injectable({
@@ -15,28 +10,11 @@ enum ConfUrl {
   })
 export class ConfigurationService {
 
-    private config: Configuration | null;
+    constructor() {}
 
-    constructor(
-        private http: HttpClient,
-        private log: LoggerService
-    ) {
-        this.fetchConfiguration();
-    }
-
-    private fetchConfiguration() {
-        return this.http.get<Configuration>(ConfUrl.configFile)
-            .subscribe(
-                (data: Configuration) => {
-                    this.config = { ... data };
-                    this.log.write(`config data loaded ${JSON.stringify(this.config)}`);
-                },
-                error => { this.log.write(error.error.message); },
-                () => { this.log.write('configuration loading complete'); }
-            );
-    }
-
-    getConfiguration(): Configuration | null {
-        return this.config;
+    getConfiguration(): Configuration {
+        return {
+            appBaseUrl: environment.appBaseUrl
+        };
     }
 }
